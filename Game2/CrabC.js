744b1cd723fb3c1ed102c45f535f9f56d158d917
anettala <anett.alamets.001@gmail.com>
2025-09-03 15:56:52 +0300

    added per second click, background images, correct coin image, fixed button shadow

--- Game2/CrabC.js
+++ Game2/CrabC.js
@@ -4,16 +4,17 @@ import Crab from "./Buyable.js";
 // per second modifier mitte click modifier
 
 let score = 0;
-let modifier = 1;
+let perSecAdd = 0;
 let addable = 1;
 
 const crabs = [
-    new Crab(1, 10, 0),
-    new Crab(2, 50, 0),
+    new Crab(0.1, 10, 0),
+    new Crab(0.3, 50, 0),
+    new Crab(1, 1000, 0),
 ];
 
 // Increment score on cookie click
-const cb = document.getElementById("crabButton");
+const cb = document.getElementById("coinButton");
 const scoreDisplay = document.getElementById('score');
 const addableDisplay = document.getElementById('addable');
 
@@ -23,16 +24,23 @@ cb.addEventListener("click", function () {
     displayScore();
 });
 
-document.getElementById("buy1").addEventListener("click", () => buyableAdd(0, "b1"));
-document.getElementById("buy2").addEventListener("click", () => buyableAdd(1, "b2"));
+document.getElementById("buy1").addEventListener("click", () => addPerSec(0, "b1"));
+document.getElementById("buy2").addEventListener("click", () => addPerSec(1, "b2"));
+document.getElementById("buy3").addEventListener("click", () => buyableAdd(2, "b3"));
 
 function displayScore() {
-    scoreDisplay.textContent = score;
-    addableDisplay.textContent = addable;
+    scoreDisplay.textContent = Math.trunc(score);
+    console.log(score, perSecAdd);
+    addableDisplay.textContent = Math.round(perSecAdd * 10) / 10
 }
 
 function addScore(amount) {
     score += amount;
+    displayScore();
+}
+
+function addIncrement() {
+    score += perSecAdd;
     displayScore();
 }
 
@@ -41,7 +49,7 @@ function buyableAdd(id, name) {
     let price = crab.getPrice();
     let amount = crab.getAmount();
 
-    if (score > price) {
+    if (score >= price) {
         addable += amount;
         score -= price;
         crab.addCrab();
@@ -54,3 +62,24 @@ function buyableAdd(id, name) {
         console.log("HAHA POOR");
     }
 }
+
+function addPerSec(id, name) {
+    let crab = crabs[id];
+    let price = crab.getPrice();
+    let amount = crab.getAmount();
+
+    if (score >= price) {
+        perSecAdd += amount;
+        score -= price;
+        crab.addCrab();
+
+        document.getElementById(name).textContent = crab.getCurrently();
+
+        console.log("Added: " + amount + " | Price: " + price);
+        displayScore();
+    } else {
+        console.log("HAHA POOR");
+    }
+}
+
+setInterval(addIncrement, 1000);
